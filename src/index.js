404a52ace750aba47d5b5b87c649ebeb02583426
sirrrik <you@example.com>
2021-07-18 09:49:18 +0300

    working front page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import {
+    Row,Col,Card,CardBody, CardText,CardTitle,CardImg
+} from 'reactstrap'
+import './index.css'
+
+class ContentFeed extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            'items':[]
+        }
+    }
+    componentDidMount() {
+        this.getItems();
+    }
+
+    getItems(){
+        fetch('http://127.0.0.1:8000/api/Item/')
+        .then(results => results.json())
+        .then(results =>this.setState({'items':results})) ;
+    }
+    render() {
+        return (
+            <ul>
+                {this.state.items.map(function(item, index){
+                    return  <ContentItem item={item} key={index}/>
+                         })}
+            </ul>
+        );
+    }
+}
+
+const ContentItem = ({item}) => (
+    <Row className="ContentItem">
+    <Col xs="3"/>
+    <Col xs="12" sm="6">
+        <Card>
+            <CardImg top width="100%" src={item.image}></CardImg>
+            <CardBody>
+                <CardTitle>
+                    {item.title}
+                </CardTitle>
+                <CardText>
+                    {item.description}
+                </CardText>
+            </CardBody>
+        </Card>
+    </Col>
+</Row>
+
+)
+
+
+ 
+
+ReactDom.render(
+    <ContentFeed/>,
+    document.getElementById('root')
+)
